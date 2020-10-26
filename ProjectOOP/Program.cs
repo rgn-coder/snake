eacd614e873289acd205921f9a70cbb49745d71c
rgn-coder <reginadryagina@gmail.com>
2020-10-26 15:36:48 +0400

    add project to repo, hello world

---
+++ ProjectOOP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
