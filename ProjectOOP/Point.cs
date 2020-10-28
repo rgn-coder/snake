74f353e4cbc3852f4992115666b91d4eae0be3ab
rgn-coder <reginadryagina@gmail.com>
2020-10-28 19:12:06 +0400

    add class point

---
+++ ProjectOOP/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SnakeGame
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+        public Point()
+            
+        {
+            
+        }
+    }
+}
