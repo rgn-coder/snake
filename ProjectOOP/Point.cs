cc87b6bfff438d2803a9ff6bf9e0083dafc7bd53
rgn-coder <reginadryagina@gmail.com>
2020-10-28 19:31:29 +0400

    использовали инкапсуляцию

--- ProjectOOP/Point.cs
+++ ProjectOOP/Point.cs
@@ -7,15 +7,28 @@ namespace SnakeGame
         public int y;
         public char sym;
 
+
+
+
+
+
+
+        public Point()
+        {
+          
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
-        public Point()
-            
-        {
-            
-        }
+        
     }
 }
