6a968ee6d55bb2d6ecd1e934413a1fc2b646df4e
rgn-coder <reginadryagina@gmail.com>
2020-10-30 15:15:00 +0400

    add List

--- ProjectOOP/Program.cs
+++ ProjectOOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnakeGame
 {
@@ -7,14 +8,31 @@ namespace SnakeGame
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-  
-            p1.Draw();
+              p1.Draw();
            
 
             Point p2 = new Point(4, 5, '#');
-          
             p2.Draw();
 
+            List<int> numList = new List<int>();  //список
+            numList.Add( 0 );
+            numList.Add( 1 );
+            numList.Add( 2 );
+
+            int x = numList[ 0 ];
+            int y = numList[ 1 ];
+            int z = numList[ 2 ];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
