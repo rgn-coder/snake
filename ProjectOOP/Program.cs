af6c1326f6b56361b46f486713ddc6fe7ead43ce
rgn-coder <reginadryagina@gmail.com>
2020-11-03 12:06:30 +0400

    add move snake

--- ProjectOOP/Program.cs
+++ ProjectOOP/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SnakeGame
 {
@@ -7,6 +11,8 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
+       //     Console.SetBufferSize(80, 25);
+            
             //Рамочка
 
 
@@ -22,11 +28,31 @@ namespace SnakeGame
 
             // отрисовка точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
-       
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.LeftArrow)
+                        snake.direction = Direction.LEFT;
+                    else if (key.Key == ConsoleKey.RightArrow)
+                        snake.direction = Direction.RIGHT;
+                    else if (key.Key == ConsoleKey.DownArrow)
+                        snake.direction = Direction.DOWN;
+                    else if (key.Key == ConsoleKey.UpArrow)
+                        snake.direction = Direction.UP;
+
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
         }
 
-        
     }  
 
 }
